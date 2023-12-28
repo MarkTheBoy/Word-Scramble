58d541b698d26ea7cd7b3ba6a4b412ba497bbad0
MarkTheBoy <146758649+MarkTheBoy@users.noreply.github.com>
2023-12-28 09:38:12 +0200

    Word Scramble with hints

---
+++ js/words.js
@@ -0,0 +1,14 @@
+const words = [
+    {
+    word: "addition",
+    hint: "The process of adding numbers"
+    },
+    {
+        word: "meeting",
+        hint : "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    }
+]
